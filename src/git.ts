43b62b53446bffc368ead76aba007ee65c08c8fc
Krystof Woldrich <krystofwoldrich@gmail.com>
2025-01-06 11:51:59 +0100

    feat(git): Enhance Git remote and config type definitions
    - Introduced `GitRemote` type for better structure of remote URL parsing.
    - Updated `parseRemoteUrl` function to return `GitRemote` type.
    - Added `GitConfig` type to define the structure of Git configuration.
    - Refined `getRemotes` function to accept `GitConfig` type instead of `any`.

--- src/git.ts
+++ src/git.ts
@@ -9,7 +9,12 @@ export function isGitRepository(dir: string): boolean {
   return false;
 }
 
-export function parseRemoteUrl(remoteUrl: string): { owner: string, name: string } | null {
+export type GitRemote = {
+  owner: string;
+  name: string;
+};
+
+export function parseRemoteUrl(remoteUrl: string): GitRemote | null {
   let [owner, name] = remoteUrl.split(':')[1].split('/');
   name = name.replace('.git', '');
 
@@ -19,11 +24,22 @@ export function parseRemoteUrl(remoteUrl: string): { owner: string, name: string
   return { owner, name };
 }
 
+export type GitConfig = {
+  [key: `branch "${string}"`]: {
+    merge: string;
+  };
+  [key: `remote "${string}"`]: {
+    url: string;
+  };
+};
+
+export type GitConfigBranch = GitConfig['branch "branch-name"'];
+export type GitConfigRemote = GitConfig['remote "remote-name"'];
+
 export function parseGitConfig(dir: string) {
   return ini.parse(fs.readFileSync(path.join(dir, '.git', 'config'), 'utf-8'));
 }
 
-// TODO: config type
-export function getRemotes(config: any) {
+export function getRemotes(config: GitConfig) {
   return Object.keys(config).filter(key => key.startsWith('remote '));
 }
